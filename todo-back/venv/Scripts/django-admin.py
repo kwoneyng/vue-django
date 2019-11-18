ce6f191d6824deeed8a3648a04f6073d222592b5
kwoneyng <nan308@naver.com>
2019-11-18 13:56:45 +0900

    initial

---
+++ todo-back/venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\student\박권응\developement\vue_django\todo-back\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
