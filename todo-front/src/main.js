5df90a7367d658f7738369b415b453e2737b6493
kwoneyng <nan308@naver.com>
2019-11-18 15:07:17 +0900

    Apply vue router

--- todo-front/src/main.js
+++ todo-front/src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router' // 폴더만 지정해 놓으면 폴더 안에 있는 index.js를 가져온다
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
